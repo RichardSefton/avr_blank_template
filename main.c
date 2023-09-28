5712e3e845507b69adebf63dadbacce624284d0e
Richard Sefton <richard.sefton@googlemail.com>
2023-09-29 00:03:48 +0100

    Blank template repo for vscode avr development.
    Uses avr-gcc for intellisense, avr-libc as a library for imports, and a Makefile (
    from somewhere else on github) to compile and hopefully flash the MCU
    
    For each project, should change the MCU, F_CPU (clock speed) and BAUD Rate in the
    makefile and the MCU and F_CPU in the c_cpp_properties.json to match each project.

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+    DDRB |= (1 << DDB5);
+    while (1)
+    {
+        PORTB |= (1 << PB5);
+        _delay_ms(1000);
+        PORTB &= ~(1 << PB5);
+        _delay_ms(1000);
+    }
+    return 0;
+}
